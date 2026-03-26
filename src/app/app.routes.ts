24c39620ba6e24dc08d7f40d2fe99728545eed2f
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-03-26 21:33:46 +0100

    feat: add default route for SurveyDashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SurveyDashboard } from './components/survey-dashboard/survey-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SurveyDashboard },
+];
