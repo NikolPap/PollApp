43cc425700b6ae621dbafe8a95f510f67078b7ef
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-03-26 21:35:41 +0100

    feat: add hero section with headline, description, and CTA button

---
+++ src/app/components/hero-and-ending/hero-and-ending.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-and-ending',
+  imports: [],
+  templateUrl: './hero-and-ending.html',
+  styleUrl: './hero-and-ending.scss',
+})
+export class HeroAndEnding {}
