60a989512c92ddd4170b13756d14d058ca54ceab
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-03-26 21:34:17 +0100

    refactor: convert AppComponent to standalone component with Header and RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { Header } from "./layout/header/header";
 import { RouterOutlet } from '@angular/router';
+ 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('PollApp');
+ 
 }
