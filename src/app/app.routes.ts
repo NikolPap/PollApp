b25fc968231315965e6c4a41f56b0d46761a5715
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-03-29 23:51:21 +0200

    Update survey detail route to include ID parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,8 @@ import { SurveyDashboard } from './components/survey-dashboard/survey-dashboard'
 import { SurveyDetail } from './components/survey-detail/survey-detail';
 import { CreateSurvey } from './components/create-survey/create-survey';
 
-export const routes: Routes = [
+export const routes: Routes =[
     { path: '', component: SurveyDashboard },
-    { path: 'surveyDetail', component: SurveyDetail },
+    { path: 'surveyDetail/:id', component: SurveyDetail },  
     { path: 'createSurvey', component: CreateSurvey}
-    
-
 ];
