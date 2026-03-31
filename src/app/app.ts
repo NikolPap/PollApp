4c4d0dc714fb106ea6b986735663b130fda7b70c
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-03-31 21:20:55 +0200

    Refactor app component: add CreateSurvey import and update background logic for navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,25 +2,27 @@ import { Component, inject } from '@angular/core';
 import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { Header } from './layout/header/header';
+import { ModalService } from './services/modal.service'; 
+import { CreateSurvey } from './components/create-survey/create-survey'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, CreateSurvey], 
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-isWhiteBg = false;
+  isWhiteBg = false;
+  
   private router = inject(Router);
+  modalService = inject(ModalService); 
 
   constructor() {
     this.router.events.pipe(
       filter((event): event is NavigationEnd => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
-      this.isWhiteBg = event.urlAfterRedirects.includes('surveyDetail') || 
-                       event.urlAfterRedirects.includes('createSurvey');
-                       
+      this.isWhiteBg = event.urlAfterRedirects.includes('surveyDetail');
     });
-}
+  }
 }
