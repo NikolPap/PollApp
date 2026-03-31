5ded449027e018d8367a5783ae9540c24773ea5f
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-03-31 21:20:36 +0200

    Refactor routes: remove create survey route and clean up routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { SurveyDashboard } from './components/survey-dashboard/survey-dashboard';
 import { SurveyDetail } from './components/survey-detail/survey-detail';
-import { CreateSurvey } from './components/create-survey/create-survey';
 
 export const routes: Routes =[
     { path: '', component: SurveyDashboard },
-    { path: 'surveyDetail/:id', component: SurveyDetail },  
-    { path: 'createSurvey', component: CreateSurvey}
+    { path: 'surveyDetail/:id', component: SurveyDetail }
 ];
