af2c6de0ada234ce6f20b90e920f73d02fa73261
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-03 17:54:11 +0200

    Entferne NgOptimizedImage-Import aus dem HeroAndEnding-Komponenten-Code

--- src/app/components/hero-and-ending/hero-and-ending.ts
+++ src/app/components/hero-and-ending/hero-and-ending.ts
@@ -1,13 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
-import { NgOptimizedImage } from '@angular/common'; // Προσθήκη
 
 @Component({
   selector: 'app-hero-and-ending',
   standalone: true,
   templateUrl: './hero-and-ending.html',
   styleUrl: './hero-and-ending.scss',
-  imports: [NgOptimizedImage],
 })
 export class HeroAndEnding {
   modalService = inject(ModalService);
